c74eabee97a5cb6338a4eff43a45b535c19d3a77
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2022-06-27 01:30:24 +0200

    Upload Assignment 1

---
+++ ASG_1/ASG_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int x;
+    bool cond;
+    // if cond variable is true then number is even, if not then number is odd
+
+    while(x!=0)
+    {
+        printf("\n\t--------------------\n");
+        printf("\n\tEnter The Number = ");
+        scanf("%d",&x);
+        cond= (((x/2)*2)==x)?true:false;
+        printf("\n\tThe Number is odd = %d \n\tThe Number is Even= %d\n",!cond,cond);
+    }
+}
