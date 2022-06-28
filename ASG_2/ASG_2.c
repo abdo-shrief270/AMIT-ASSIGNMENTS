d8d164b4c374453e7b6c694c28026b719725d36e
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2022-06-28 20:34:25 +0200

    Update ASG_2

--- ASG_2/ASG_2.c
+++ ASG_2/ASG_2.c
@@ -5,8 +5,13 @@ int main()
     float num_1,num_2;
     char op;
     printf("\n\t--------------------\n");
-    printf("\tEnter the Equation [like 1+1] : \n\t\t");
-    scanf("%f%c%f",&num_1,&op,&num_2);
+    printf("\tEnter the First Number \n\t");
+    scanf("%f",&num_1);
+    printf("\tEnter the Operator \n\t");
+    scanf("\n%c",&op);
+    printf("\tEnter the Second Number \n\t");
+    scanf("%f",&num_2);
+
     switch(op)
     {
     case '+':
@@ -27,6 +32,11 @@ int main()
     case '/':
     {
         printf("\n\t\tThe Result = %.2f\n",num_1/num_2);
+        break;
+    }
+    case '%':
+    {
+        printf("\n\t\tThe Result = %d\n",((int)num_1)%((int)num_2));
         break;
     }
     default:
